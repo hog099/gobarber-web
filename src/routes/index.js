aa577c3c0f4fd351d055e3332d1ae35ddbd0d2c8
hog099 <oliveiraginelli@gmail.com>
2019-09-24 21:02:16 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Switch} from 'react-router-dom';
+import Route from './Route';
+
+import SignIn from '../pages/SignIn/index';
+import SignUp from '../pages/SignUp/index';
+
+import Dashboard from '../pages/Dashboard/index';
+import Profile from '../pages/Profile/index';
+
+export default function Routes(){
+    return (
+        <Switch>
+            <Route path="/" exact component={SignIn} />
+            <Route path="/register" component={SignUp} />
+            
+            <Route path="/dashboard" component={Dashboard} isPrivate/>
+            <Route path="/profile" component={Profile} isPrivate />
+
+            <Route path="/" component={()=><h1>404</h1>} />
+
+        </Switch>
+    );
+}
